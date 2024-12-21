80f155ac29dae4a29b446afbd1e4265494bab94c
Daniel Salvador <drante81@gmail.com>
2024-12-20 21:28:57 -0500

    inicio

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,32 @@
 package org.example;
 
+import javax.swing.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        String[] opciones = {"Crear cuenta","Acceder","Actualizar datos","Eliminar cuenta", "Finalizar"};
+        int opcion = -1;
+        do{
+           opcion = JOptionPane.showConfirmDialog(null,"¿Qué quieres hacer hoy?",
+           "Bienvenido",JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
 
+           switch (opcion){
+               case 0:
+                   break;
+               case 1:
+                   break;
+               case 2:
+                   break;
+               case 3:
+                   break;
+               case 4:
+                   break;
+               default:
+                   break;
+           }
+
+        }while(opcion !=5);
     }
 }
